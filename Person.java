5e8d058c64ddb746de6b5829541de04be12c17f6
schmitt <j.schmitt@gymnasium-veitshoechheim.de>
2017-07-04 22:36:21 +0200

    Gemeinsames Projekt angelegt

---
+++ Person.java
@@ -0,0 +1,37 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+
+/**
+ * 
+ */
+public class Person
+{
+    private int alter;
+    private String name;
+    private String beruf;
+
+    /**
+     * 
+     */
+    public Person(String name, String beruf, int alter)
+    {
+        this.name = name;
+        this.beruf = beruf;
+        this.alter = alter;
+    }
+
+    /**
+     * 
+     */
+    public int gibAlter()
+    {
+        return alter;
+    }
+
+    /**
+     * 
+     */
+    public void habeGeburtstag()
+    {
+        alter = alter + 1;
+    }
+}
